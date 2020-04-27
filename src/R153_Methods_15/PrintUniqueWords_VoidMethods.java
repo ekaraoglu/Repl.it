c597fb2c5a6d9727a071ac05d1a94bf71a51790e
ekaraoglu <emrekaraoglu@msn.com>
2020-04-26 18:36:22 -0600

    R153_Methods_15

---
+++ src/R153_Methods_15/PrintUniqueWords_VoidMethods.java
@@ -0,0 +1,50 @@
+package R153_Methods_15;
+
+import java.util.Scanner;
+
+public class PrintUniqueWords_VoidMethods {
+    /*
+    Complete a void method printUniqueWords() that will print only unique words from an array of strings.
+    Every single unique word should be printed from the new line.
+
+Example:
+input:[java, code, python, code, rust, code, rust]
+output:
+java
+python
+     */
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        int size = scan.nextInt();
+        String[] words = new String[size];
+        for(int i=0; i < size; i++){
+            words[i] = scan.next();
+        }
+        printUniqueWords(words);
+    }
+
+    public static void printUniqueWords(String[] words){
+        //WRITE YOUR CODE HERE
+
+              for (int i = 0; i < words.length; i++){
+
+            int count = 0;
+
+            for (String each : words) {
+
+                if (words[i].equals(each)){
+
+                    count++;
+                }
+
+            }
+
+            if (count == 1){
+
+                System.out.println(words[i]);
+            }
+        }
+
+    }
+}
