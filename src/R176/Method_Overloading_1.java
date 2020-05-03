3c3df3d659cbbc2cc8e5c830f2c5397d06e7fd95
ekaraoglu <emrekaraoglu@msn.com>
2020-05-03 01:21:00 -0600

    R182_MethodsWithArrayList4_Set

---
+++ src/R176/Method_Overloading_1.java
@@ -0,0 +1,12 @@
+package R176;
+
+public class Method_Overloading_1 {
+    /*
+    In this task, you need to write 2 methods findMax() that will find a maximum number in the array. First method should work with array of integers (int[]) and return int, and second method should work with an array of doubles (double[]) and return double as a result.
+Methods must have the same name and different parameters.
+Fo more information:  https://docs.oracle.com/javase/tutorial/java/javaOO/methods.html
+
+Comment: Methods should be non static and with a return type.
+     */
+
+}
---
+++ src/R181_MethodsWithArrayList_3/CombineArrays.java
@@ -0,0 +1,46 @@
+package R181_MethodsWithArrayList_3;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class CombineArrays {
+    /*
+    combine two String arrays into one arraylist and return it as a string.
+
+for example:
+
+combineRs(["f","o","o"],[" b","a","r"])
+returns "foo bar"
+
+
+combineRs(["1","2","3"],[" 4"])
+returns "1234"
+     */
+    /*
+    public static ArrayList<String> combineRs(String[] r1, String[] r2){
+
+    ArrayList<String> combineRs = new ArrayList<>();
+
+    for (int i = 0;  i < r1.length; i++){
+
+        String each = r1[i];
+
+        combineRs.add(each);
+    }
+
+    for (String each : r2){
+
+        combineRs.add(each);
+    }
+
+
+
+return ;
+
+
+
+
+
+    }
+*/
+
+}
